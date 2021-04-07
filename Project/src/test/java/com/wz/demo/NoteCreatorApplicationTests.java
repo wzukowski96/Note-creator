fae164ff6c17145648d21f33f506d1f309a10de6
Wojtek Zuk <wzukowski96@gmail.com>
2021-04-07 15:48:28 +0200

    Initialization

---
+++ Project/src/test/java/com/wz/demo/NoteCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
